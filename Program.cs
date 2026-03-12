453315805e3d246fa565004664b003f076657813
Chris Amaral <chris_lla@hotmail.com>
2026-03-12 17:52:41 -0300

    Implementa classes Iphone e Nokia herdando de Smartphone e sobrescreve método InstalarAplicativo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,34 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Nokia nokia = new Nokia
+        (
+            "1165753589",
+            "Nokia X",
+            "12244466666",
+            64
+        );
+
+        Iphone iphone = new Iphone
+        (
+            "11999999999",
+            "iPhone X",
+            "23344477777",
+            128
+        );
+        Console.WriteLine("Nokia:");
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("/n");
+
+        Console.WriteLine("iPhone:");
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
